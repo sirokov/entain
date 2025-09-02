086d7840ed2e2b5012c6623dccbc4d09de529181
Roman Shirokov <sirokov@yahoo.no>
2025-09-02 11:08:29 +0300

    Remove unused constant SPORT_NOT_EMPTY_ERROR
    The constant `SPORT_NOT_EMPTY_ERROR` was removed as it is no longer used in the codebase. This cleanup improves maintainability by getting rid of redundant code.

--- src/main/java/com/entain/config/EntainConstant.java
+++ src/main/java/com/entain/config/EntainConstant.java
@@ -8,7 +8,6 @@ public interface EntainConstant {
     String SPORT_EVENT =  "Sport event with id ";
     String NOT_FOUND =  " not found";
     String INVALID_STATUS_CHANGE_ATTEMPT =  "Invalid status change operation according to business rules";
-    String SPORT_NOT_EMPTY_ERROR =  "Sport cannot be null or empty";
     String INVALID_SPORT =  "Invalid sport: ";
     String ALLOWED =  ". Allowed: ";
     String THREAD_POOL_FULL_ERROR =  "Async task rejected: thread pool is full! Active threads: ";
